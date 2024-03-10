4ebbdda086a30fa12ba4077a8f8fbe85b399be16
Mujahid_Sadiq <subhanmustafai1234@gmail.com>
2024-03-10 15:00:10 +0500

    build a class for item which i will show on the catalog app front page

---
+++ lib/models/catalog.dart
@@ -0,0 +1,24 @@
+
+
+class item {
+  final String id;
+  final String name;
+  final String desc;
+  final String  price;
+  final String  color;
+  final String  image;
+
+  item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+}
+
+final products=[
+  item(
+  color: 'white',
+  id: '1',
+  name: 'a',
+  desc: '1',
+  price: '2',
+  image: 'a',
+
+  )
+]
